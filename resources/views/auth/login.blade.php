b7edb1c53eb46966684131027d4baa4723c0e512
Sammy Joseph <sam.tab.paz@gmail.com>
2024-10-10 06:02:51 -0500

    login: reemplazo de diseño del form default de jetstream

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,10 @@
 <x-guest-layout>
     <div class="bg-gray-50">
         <div class="max-w-6xl mx-auto grid grid-cols-2 items-center">
+            {{-- Columna 1 - Formulario --}}
             <section class="dark:bg-gray-900">
                 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    {{-- Logo --}}
                     <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img class="max-w-32" src="{{ asset('images/logo.png') }}" alt="logo">                        
                     </a>
@@ -11,42 +13,46 @@
                             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Inicia sesión en tu cuenta
                             </h1>
-                            <form class="space-y-4 md:space-y-6" action="#">
+                            <form method="POST" action="{{ route('login') }}" class="space-y-4 md:space-y-6">
+                                @csrf
                                 <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tu email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ejemplo@gmail.com" required="">
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ejemplo@gmail.com" :value="old('email')" required autofocus autocomplete="username">
                                 </div>
                                 <div>
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required autocomplete="current-password">
                                 </div>
                                 <div class="flex items-center justify-between">
                                     <div class="flex items-start">
                                         <div class="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="">
+                                            <input id="remember_me" name="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                                         </div>
                                         <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Recordar mi sesión</label>
+                                            <label for="remember" class="text-gray-500 dark:text-gray-300">Recordar mi sesión</label>
                                         </div>
                                     </div>
-                                    <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">¿Perdiste tu contraseña?</a>
+                                    @if (Route::has('password.request'))
+                                        <a href="{{ route('password.request') }}" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">¿Perdiste tu contraseña?</a>
+                                    @endif
                                 </div>
-                                <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+                                <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Iniciar sesión</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    ¿No estás registrado aún? <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500 underline">Crea tu cuenta aquí</a>
+                                    ¿No estás registrado aún? <a href="{{ route('register') }}" class="font-medium text-blue-600 hover:underline dark:text-blue-500 underline">Crea tu cuenta aquí</a>
                                 </p>
                             </form>
                         </div>
                     </div>
                 </div>
             </section>
+            {{-- Columna 2 - Gráfico --}}
             <div>
                 <img src="{{ asset('images/login_illustration.svg') }}" alt="">
             </div>
         </div>
     </div>
 
-    <x-authentication-card>
+    {{-- <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
@@ -91,5 +97,5 @@
                 </x-button>
             </div>
         </form>
-    </x-authentication-card>
+    </x-authentication-card> --}}
 </x-guest-layout>
